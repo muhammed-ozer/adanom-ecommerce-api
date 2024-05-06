005928c8383951eaa8108058c1a428f6dfa4e25e
Muhammed Ozer <hello@muhammedozer.com>
2024-05-06 18:43:02 +0300

    Add installment properties to product sku table (#86ep9b9c7)

---
+++ src/Adanom.Ecommerce.API.Data/Migrations/20240506154147_AddInstallmentPropertiesToProductSKUTable.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Adanom.Ecommerce.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInstallmentPropertiesToProductSKUTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEligibleToInstallment",
+                table: "ProductSKUs",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "MaximumInstallmentCount",
+                table: "ProductSKUs",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsEligibleToInstallment",
+                table: "ProductSKUs");
+
+            migrationBuilder.DropColumn(
+                name: "MaximumInstallmentCount",
+                table: "ProductSKUs");
+        }
+    }
+}
