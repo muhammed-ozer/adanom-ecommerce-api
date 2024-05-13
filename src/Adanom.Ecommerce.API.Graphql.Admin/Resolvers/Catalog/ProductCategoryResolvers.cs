ee955c682585d875ead64cb3b0319d761f7fe448
Muhammed Ozer <hello@muhammedozer.com>
2024-05-13 15:57:23 +0300

    Implement get product category parent resolver (#86epakhp8)

---
+++ src/Adanom.Ecommerce.API.Graphql.Admin/Resolvers/Catalog/ProductCategoryResolvers.cs
@@ -0,0 +1,25 @@
+namespace Adanom.Ecommerce.API.Graphql.Admin.Resolvers
+{
+    [ExtendObjectType(typeof(ProductCategoryResponse))]
+    public sealed class ProductCategoryResolvers
+    {
+        #region GetParentAsync
+
+        public async Task<ProductCategoryResponse?> GetParentAsync(
+           [Parent] ProductCategoryResponse productCategoryResponse,
+           [Service] IMediator mediator)
+        {
+            if (productCategoryResponse.ParentId == null)
+            {
+                return null;
+            }
+
+            var parent = await mediator.Send(new GetProductCategoryById(productCategoryResponse.ParentId.Value));
+
+            return parent;
+        }
+
+        #endregion
+
+    }
+}
