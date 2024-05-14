98ea289cbcb88d616f92676d3f831d237f466856
Muhammed Ozer <hello@muhammedozer.com>
2024-05-14 09:25:09 +0300

    Change meta information title column string length (#86ep8dz1x)

--- src/Adanom.Ecommerce.API.Data/Migrations/20240514062209_ChangeMetaInformationTitleColumnLength.Designer.cs
+++ src/Adanom.Ecommerce.API.Data/Migrations/20240514062209_ChangeMetaInformationTitleColumnLength.Designer.cs
@@ -4,6 +4,7 @@ using Adanom.Ecommerce.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Adanom.Ecommerce.API.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240514062209_ChangeMetaInformationTitleColumnLength")]
+    partial class ChangeMetaInformationTitleColumnLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
