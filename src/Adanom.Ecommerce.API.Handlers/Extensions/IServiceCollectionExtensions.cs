497241ecd313768daa5c3234cd8827f45200f4fe
Muhammed Ozer <62310850+muhammed-ozer@users.noreply.github.com>
2024-05-17 18:51:11 +0300

    Implement commit transaction behavior (#86epcemxc)

--- src/Adanom.Ecommerce.API.Handlers/Extensions/IServiceCollectionExtensions.cs
+++ src/Adanom.Ecommerce.API.Handlers/Extensions/IServiceCollectionExtensions.cs
@@ -22,6 +22,7 @@ namespace Microsoft.Extensions.DependencyInjection
                 options.AddBehavior<IPipelineBehavior<CreateBrand, BrandResponse?>, CreateBrand_CommitTransactionBehavior>();
                 options.AddBehavior<IPipelineBehavior<CreateBrand, BrandResponse?>, CreateBrand_CreateMetaInformationBehavior>();
 
+                options.AddBehavior<IPipelineBehavior<DeleteBrand, bool>, DeleteBrand_CommitTransactionBehavior>();
                 options.AddBehavior<IPipelineBehavior<DeleteBrand, bool>, DeleteBrand_DeleteRelationsBehavior>();
 
                 #endregion
---
+++ src/Adanom.Ecommerce.API.Handlers/Handlers/Catalog/Brand/DeleteBrand_CommitTransactionBehavior.cs
@@ -0,0 +1,82 @@
+﻿using System.Security.Claims;
+
+namespace Adanom.Ecommerce.API.Handlers
+{
+    public sealed class DeleteBrand_CommitTransactionBehavior : IPipelineBehavior<DeleteBrand, bool>
+    {
+        #region Fields
+
+        private readonly ApplicationDbContext _applicationDbContext;
+        private readonly IMediator _mediator;
+
+        #endregion
+
+        #region Ctor
+
+        public DeleteBrand_CommitTransactionBehavior(ApplicationDbContext applicationDbContext, IMediator mediator)
+        {
+            _applicationDbContext = applicationDbContext ?? throw new ArgumentNullException(nameof(applicationDbContext));
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+
+        }
+
+        #endregion
+
+        #region IPipelineBehavior Members
+
+        public async Task<bool> Handle(DeleteBrand command, RequestHandlerDelegate<bool> next, CancellationToken cancellationToken)
+        {
+            var deleteBrandResponse = await next();
+
+            var currentTransaction = _applicationDbContext.Database.CurrentTransaction;
+
+            if (!deleteBrandResponse)
+            {
+                if (currentTransaction != null)
+                {
+                    await currentTransaction.RollbackAsync(cancellationToken);
+
+                    await _mediator.Publish(new CreateLog(new AdminTransactionLogRequest()
+                    {
+                        UserId = command.Identity.GetUserId(),
+                        EntityType = EntityType.BRAND,
+                        TransactionType = TransactionType.DELETE,
+                        Description = LogMessages.AdminTransaction.DatabaseTransactionHasFailed,
+                    }));
+                }
+
+                return false;
+            }
+
+            try
+            {
+                if (currentTransaction != null)
+                {
+                    await currentTransaction.CommitAsync();
+                }
+            }
+            catch (Exception exception)
+            {
+                if (currentTransaction != null)
+                {
+                    await currentTransaction.RollbackAsync(cancellationToken);
+                }
+
+                await _mediator.Publish(new CreateLog(new AdminTransactionLogRequest()
+                {
+                    UserId = command.Identity.GetUserId(),
+                    EntityType = EntityType.BRAND,
+                    TransactionType = TransactionType.DELETE,
+                    Description = LogMessages.AdminTransaction.DatabaseTransactionHasFailed,
+                    Exception = exception.ToString()
+                }));
+
+                deleteBrandResponse = false;
+            }
+
+            return deleteBrandResponse;
+        }
+
+        #endregion
+    }
+}
