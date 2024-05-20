bd2c0afff403e218a22a280dcdb44ff29671db1d
Muhammed Ozer <hello@muhammedozer.com>
2024-05-20 18:31:15 +0300

    Implement order status type API (ADNAPI-82)

---
+++ src/Adanom.Ecommerce.API.Handlers/Handlers/Order/OrderStatusType/GetOrderStatusTypeHandler.cs
@@ -0,0 +1,32 @@
+namespace Adanom.Ecommerce.API.Handlers
+{
+    public sealed class GetOrderStatusTypeHandler : IRequestHandler<GetOrderStatusType, OrderStatusTypeResponse?>
+
+    {
+        #region Fields
+
+        private readonly IMediator _mediator;
+
+        #endregion
+
+        #region Ctor
+
+        public GetOrderStatusTypeHandler(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        #endregion
+
+        #region IRequestHandler Members
+
+        public async Task<OrderStatusTypeResponse?> Handle(GetOrderStatusType command, CancellationToken cancellationToken)
+        {
+            var orderStatusTypes = await _mediator.Send(new GetOrderStatusTypes());
+
+            return orderStatusTypes.SingleOrDefault(e => e.Key == command.OrderStatusType);
+        }
+
+        #endregion
+    }
+}
