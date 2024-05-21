11ebd78ceb3b19b79472a007249a34b57cc669dc
Muhammed Ozer <62310850+muhammed-ozer@users.noreply.github.com>
2024-05-21 15:47:18 +0300

    Remove unnecessary properties (ADNAPI-100)

--- src/Adanom.Ecommerce.API.Services.Mail/MailRequest.cs
+++ src/Adanom.Ecommerce.API.Services.Mail/MailRequest.cs
@@ -4,8 +4,6 @@ namespace Adanom.Ecommerce.API.Services.Mail
 {
     public sealed class MailRequest
     {
-        public MailTemplateKey Key { get; set; }
-
         public string To { get; set; } = null!;
 
         public string Subject { get; set; } = null!;
@@ -15,8 +13,6 @@ namespace Adanom.Ecommerce.API.Services.Mail
         public string? Cc { get; set; }
 
         public string? Bcc { get; set; }
-
-        public IDictionary<string, string>? Replacements { get; set; }
 
         public IEnumerable<UploadedFile>? Attachments { get; set; }
     }
