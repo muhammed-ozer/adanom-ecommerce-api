82f33e50598226707eb36b5827dfea8cd90726d9
Muhammed Ozer <hello@muhammedozer.com>
2024-05-21 15:21:00 +0300

    Add mail keys and constants (ADNAPI-82)

---
+++ src/Adanom.Ecommerce.API.Services.Mail/MailConstants.cs
@@ -0,0 +1,21 @@
+﻿namespace Adanom.Ecommerce.API.Services.Mail
+{
+    public static class MailConstants
+    {
+        public static class Replacements
+        {
+            public static class User
+            {
+                public const string FullName = "{USER_FULL_NAME}";
+
+            }
+
+            public static class Order
+            {
+                public const string Number = "{ORDER_NUMBER}";
+                public const string ShippingTrackingCode = "{ORDER_SHIPPING_TRACKING_CODE}";
+                public const string ShippingProviderName = "{ORDER_SHIPPING_PROVIDER_NAME}";
+            }
+        }
+    }
+}
