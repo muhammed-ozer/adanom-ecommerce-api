34186cf53f5ced6232581873567e73668d7a4243
Muhammed Ozer <hello@muhammedozer.com>
2024-06-03 14:03:36 +0300

    Implement CORS policy (ADNAPI-112)

---
+++ src/Adanom.Ecommerce.API.Site/SiteConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace Adanom.Ecommerce.API.Site
+{
+    internal static class SiteConstants
+    {
+        internal static class CORSPolicyNames
+        {
+            public const string Default = "DefaultPolicy";
+        }
+    }
+}
