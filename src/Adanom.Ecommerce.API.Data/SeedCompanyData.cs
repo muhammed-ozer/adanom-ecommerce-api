affb1fd4173aea754d14db1004e8b403100190d9
Muhammed Ozer <hello@muhammedozer.com>
2024-07-11 19:01:47 +0300

    Add seed data files and implement later (ADNAPI-126)

---
+++ src/Adanom.Ecommerce.API.Data/SeedCompanyData.cs
@@ -0,0 +1,26 @@
+﻿using Adanom.Ecommerce.API.Data.Models;
+
+namespace Adanom.Ecommerce.API.Data
+{
+    internal static class SeedCompanyData
+    {
+        internal static Company Company { get; set; }
+
+        static SeedCompanyData()
+        {
+            Company = new Company()
+            {
+                LegalName = "Adanom Mağazacılık Sanayi ve Ticaret Limited Şirketi",
+                DisplayName = "Adanom",
+                Address = "Topraklık mahallesi Halk Caddesi No: 21/A",
+                AddressCityId = 20,
+                AddressDistrictId = 361,
+                PhoneNumber = "8503462366",
+                Email = "info@adanom.com",
+                TaxAdministrationId = 0,
+                TaxNumber = "0071233691",
+                MersisNumber = "0007123369100001"
+            };
+        }
+    }
+}
