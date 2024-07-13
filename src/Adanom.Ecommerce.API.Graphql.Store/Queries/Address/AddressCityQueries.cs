13f8c9af4deb393e3ee53cf5d0e1f55864ce5c59
Muhammed Ozer <62310850+muhammed-ozer@users.noreply.github.com>
2024-07-13 09:37:29 +0300

    Implement address city and district APIs (ADNAPI-138)

---
+++ src/Adanom.Ecommerce.API.Graphql.Store/Queries/Address/AddressCityQueries.cs
@@ -0,0 +1,33 @@
+namespace Adanom.Ecommerce.API.Graphql.Store.Queries
+{
+    [ExtendObjectType(OperationTypeNames.Query)]
+    public class AddressCityQueries
+    {
+        #region GetAddressCityAsync
+
+        [GraphQLDescription("Gets address city")]
+        public async Task<AddressCityResponse?> GetAddressCityAsync(
+            long id,
+            [Service] IMediator mediator)
+        {
+            var command = new GetAddressCity(id);
+
+            return await mediator.Send(command);
+        }
+
+        #endregion
+
+        #region GetAddressCitiesAsync
+
+        [GraphQLDescription("Gets address cities")]
+        public async Task<IEnumerable<AddressCityResponse>> GetAddressCitiesAsync(
+            [Service] IMediator mediator)
+        {
+            var command = new GetAddressCities();
+
+            return await mediator.Send(command);
+        }
+
+        #endregion
+    }
+}
