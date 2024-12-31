372da0d8ca12258758ff0f804af5b464bfa286dc
Muhammed Ozer <hello@muhammedozer.com>
2024-12-31 18:27:56 +0300

    Add price columns to anonymous shopping cart item table (ADNAPI-185)

---
+++ src/Adanom.Ecommerce.API.Data/Migrations/20241231152011_AddPriceColumnsToAnonymousShoppingCartItemTable.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Adanom.Ecommerce.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPriceColumnsToAnonymousShoppingCartItemTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "DiscountedPrice",
+                table: "AnonymousShoppingCartItems",
+                type: "decimal(10,2)",
+                precision: 10,
+                scale: 2,
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "OriginalPrice",
+                table: "AnonymousShoppingCartItems",
+                type: "decimal(10,2)",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DiscountedPrice",
+                table: "AnonymousShoppingCartItems");
+
+            migrationBuilder.DropColumn(
+                name: "OriginalPrice",
+                table: "AnonymousShoppingCartItems");
+        }
+    }
+}
