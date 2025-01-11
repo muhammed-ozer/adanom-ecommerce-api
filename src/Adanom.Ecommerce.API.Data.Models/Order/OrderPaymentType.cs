fbb99c52e07f5bb1189cca1d8ba8db589dbb57b1
Muhammed Ozer <hello@muhammedozer.com>
2025-01-11 16:43:47 +0300

    Add order payment type to db (ADNAPI-193)

---
+++ src/Adanom.Ecommerce.API.Data.Models/Order/OrderPaymentType.cs
@@ -0,0 +1,19 @@
+﻿using Adanom.Ecommerce.API.Data.Attributes;
+
+namespace Adanom.Ecommerce.API.Data.Models
+{
+    public enum OrderPaymentType : byte
+    {
+        [EnumDisplayName("Kredi Kartı")]
+        ONLINE_CREDIT_CARD,
+
+        [EnumDisplayName("Havale/EFT")]
+        BANK_TRANSFER,
+
+        [EnumDisplayName("Teslimatta Kredi Kartı")]
+        CREDIT_CARD_ON_DELIVERY,
+
+        [EnumDisplayName("Teslimatta Nakit")]
+        CASH_ON_DELIVERY
+    }
+}
---
+++ src/Adanom.Ecommerce.API.Data/Migrations/20250111133851_AddOrderPaymentTypeEnumAndAddNewColumnsToOrderPaymentTable.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Adanom.Ecommerce.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrderPaymentTypeEnumAndAddNewColumnsToOrderPaymentTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TransactionId",
+                table: "OrderPayments",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GatewayResponse",
+                table: "OrderPayments",
+                type: "nvarchar(max)",
+                maxLength: 5000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GatewayInitializationResponse",
+                table: "OrderPayments",
+                type: "nvarchar(max)",
+                maxLength: 5000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "OrderPaymentType",
+                table: "OrderPayments",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Paid",
+                table: "OrderPayments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PaidAtUtc",
+                table: "OrderPayments",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderPaymentType",
+                table: "OrderPayments");
+
+            migrationBuilder.DropColumn(
+                name: "Paid",
+                table: "OrderPayments");
+
+            migrationBuilder.DropColumn(
+                name: "PaidAtUtc",
+                table: "OrderPayments");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TransactionId",
+                table: "OrderPayments",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GatewayResponse",
+                table: "OrderPayments",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldMaxLength: 5000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GatewayInitializationResponse",
+                table: "OrderPayments",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldMaxLength: 5000,
+                oldNullable: true);
+        }
+    }
+}
