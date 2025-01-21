526025d0e3e6f495237a370988c5abf426694a81
Muhammed Ozer <hello@muhammedozer.com>
2025-01-21 14:14:57 +0300

    Update delivered at utc when order done (ADNAPI-209)

--- src/Adanom.Ecommerce.API.Commands/Commands/Order/Order/UpdateOrder_OrderStatusType.cs
+++ src/Adanom.Ecommerce.API.Commands/Commands/Order/Order/UpdateOrder_OrderStatusType.cs
@@ -30,6 +30,8 @@ namespace Adanom.Ecommerce.API.Commands
 
         public string? ShippingTrackingCode { get; set; }
 
+        public DateTime? DeliveredAtUtc { get; set; }
+
         #endregion
     }
 }
